0264df4deb4699a19ffccc54b6fddfef8091147e
bluemangoo <chenfy2006@qq.com>
2023-02-20 13:52:33 +0800

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,6 @@
-import Plugin from "./types/plugin";
-import Response from "./types/response";
+import VCLight from "./VCLight";
 
-export default class VCLight {
-    constructor(config: {} = {}) {
-        this.config = config;
-    }
+export default VCLight;
 
-    readonly config: {};
-    plugins: Plugin[] = [];
-
-    public use(plugin: Plugin) {
-        this.plugins[this.plugins.length] = plugin;
-    }
-
-    private broken = false;
-
-    public breakApp() {
-        this.broken = true;
-    }
-
-    private sendResponse(response: any, responseContent: Response) {
-        if (responseContent.redirect) {
-            response.redirect(responseContent.redirectUrl, responseContent.status);
-            return;
-        }
-        response.status(responseContent.status).send(responseContent.response);
-    }
-
-    public async fetch(request: any, response: any) {
-        let responseContent: Response = new Response();
-
-        for (const pluginsKey in this.plugins) {
-            await this.plugins[pluginsKey].init();
-            if(this.broken){
-                this.sendResponse(response,responseContent);
-                return
-            }
-        }
-
-        for (const pluginsKey in this.plugins) {
-            await this.plugins[pluginsKey].process(request, response, responseContent);
-            if(this.broken){
-                this.sendResponse(response,responseContent);
-                return
-            }
-        }
-
-        for (const pluginsKey in this.plugins) {
-            await this.plugins[pluginsKey].after(request, response, responseContent);
-            if(this.broken){
-                this.sendResponse(response,responseContent);
-                return
-            }
-        }
-
-        this.sendResponse(response, responseContent);
-    }
-}
+export { default as Plugin } from "./types/plugin";
+export { default as Response } from "./types/response";
