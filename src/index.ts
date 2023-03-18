91e3ec1d42b213fb4314b3fc7b362a97af7714ca
bluemangoo <chenfy2006@qq.com>
2023-03-18 19:17:29 +0800

    fix: export builder

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export default VCLight;
 
 export { default as Plugin } from "./types/plugin";
 export { default as Response } from "./types/response";
+export { default as ResponseBuilder } from "./types/responseBuilder";
+export { default as HtmlBuilder } from "./types/responseBuilders/htmlBuilder";
