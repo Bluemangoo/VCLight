166c1a850fffc8a8de245730a41ef8d84cf38712
Bluemangoo <chenfy2006@qq.com>
2024-06-01 19:10:29 +0800

    feat(vclight): update export

--- packages/vclight/src/index.ts
+++ packages/vclight/src/index.ts
@@ -3,6 +3,8 @@ import VCLight from "./VCLight";
 export default VCLight;
 
 export { default as VCLightMiddleware } from "./types/VCLightMiddleware";
+export { default as VCLightRequest } from "./types/VCLightRequest";
 export { default as VCLightResponse } from "./types/VCLightResponse";
 export { default as ResponseBuilder } from "./types/responseBuilder";
 export { default as HtmlBuilder } from "./types/responseBuilders/htmlBuilder";
+export * from "./types/rawRequest";
